6ba81787f81e0a37a2c97a010b3f4b9ff54118fe
Iain <iain@zora.co>
2021-09-14 09:47:37 -0400

    overall code cleanup

--- contracts/archive/ISerialMultipleMintable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity 0.8.6;
-
-interface ISerialMultipleMintable {
-  function mintSerial(uint256 serialId, address to) external returns (uint256);
-  function mintSerials(uint256 serialId, address[] memory to) external returns (uint256);
-}

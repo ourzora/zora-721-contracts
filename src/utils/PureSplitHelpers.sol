15ed62bc1b65624141720a88a843b4fadb17fd2e
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-21 10:36:03 -0300

    I update solidity version to 0.8.15

--- src/utils/PureSplitHelpers.sol
+++ src/utils/PureSplitHelpers.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "../interfaces/ISplitMain.sol";
 
 contract PureHelpers {
     /// @notice Returns sorted array of accounts for 0xSplits.
-    function _sortAddresses(address[] memory addresses) internal pure returns (address[] memory) {
-        for (uint256 i = addresses.length - 1; i > 0;) {
-            for (uint256 j = 0; j < i;) {
+    function _sortAddresses(address[] memory addresses)
+        internal
+        pure
+        returns (address[] memory)
+    {
+        for (uint256 i = addresses.length - 1; i > 0; ) {
+            for (uint256 j = 0; j < i; ) {
                 if (addresses[i] < addresses[j]) {
                     (addresses[i], addresses[j]) = (addresses[j], addresses[i]);
                 }
@@ -26,12 +30,16 @@ contract PureHelpers {
 
     /// @notice Returns sorted array of accounts for 0xSplits.
     /// @dev sortedAccounts _must_ be sorted for this to work properly
-    function _uniqueAddresses(address[] memory sortedAccounts) internal pure returns (address[] memory) {
+    function _uniqueAddresses(address[] memory sortedAccounts)
+        internal
+        pure
+        returns (address[] memory)
+    {
         uint32 numUniqRecipients = _countUniqueRecipients(sortedAccounts);
         address[] memory _uniqueRecipients = new address[](numUniqRecipients);
         _uniqueRecipients[0] = sortedAccounts[0];
         uint32 j = 1;
-        for (uint32 i = 1; i < numUniqRecipients;) {
+        for (uint32 i = 1; i < numUniqRecipients; ) {
             while (_uniqueRecipients[i - 1] == sortedAccounts[j]) {
                 unchecked {
                     ++j;
@@ -48,11 +56,15 @@ contract PureHelpers {
 
     /// @notice Returns number of unique recipients.
     /// @dev sortedAccounts _must_ be sorted for this to work properly
-    function _countUniqueRecipients(address[] memory sortedAccounts) internal pure returns (uint32) {
+    function _countUniqueRecipients(address[] memory sortedAccounts)
+        internal
+        pure
+        returns (uint32)
+    {
         uint32 numRecipients = uint32(sortedAccounts.length);
         uint32 numUniqRecipients = 1;
         address lastRecipient = sortedAccounts[0];
-        for (uint256 i = 1; i < numRecipients;) {
+        for (uint256 i = 1; i < numRecipients; ) {
             if (sortedAccounts[i] != lastRecipient) {
                 unchecked {
                     ++numUniqRecipients;
--- src/utils/SplitHelpers.sol
+++ src/utils/SplitHelpers.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "../interfaces/ISplitMain.sol";

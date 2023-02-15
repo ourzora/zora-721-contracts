bef968f2c41d57160b1152f8cb0709975d6a3c65
Iain <iain@zora.co>
2023-02-15 15:38:53 -0500

    wip

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -18,47 +18,6 @@ import {EditionMetadataRenderer} from "../src/metadata/EditionMetadataRenderer.s
 contract Deploy is Script {
     using Strings for uint256;
 
-    function setupBlockedOperators(address subscriptionOwner)
-        public
-        returns (OwnedSubscriptionManager)
-    {
-        OwnedSubscriptionManager ownedSubscriptionManager = new OwnedSubscriptionManager(
-                subscriptionOwner
-            );
-        address[] memory blockedOperatorsList = new address[](7);
-        blockedOperatorsList[0] = address(
-            0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e
-        );
-        blockedOperatorsList[1] = address(
-            0x024aC22ACdB367a3ae52A3D94aC6649fdc1f0779
-        );
-        blockedOperatorsList[2] = address(
-            0xFED24eC7E22f573c2e08AEF55aA6797Ca2b3A051
-        );
-        blockedOperatorsList[3] = address(
-            0x00000000000111AbE46ff893f3B2fdF1F759a8A8
-        );
-        blockedOperatorsList[4] = address(
-            0x59728544B08AB483533076417FbBB2fD0B17CE3a
-        );
-        blockedOperatorsList[5] = address(
-            0xF849de01B080aDC3A814FaBE1E2087475cF2E354
-        );
-        blockedOperatorsList[6] = address(
-            0x2B2e8cDA09bBA9660dCA5cB6233787738Ad68329
-        );
-
-        IOperatorFilterRegistry operatorFilterRegistry = IOperatorFilterRegistry(
-                0x000000000000AAeB6D7670E522A718067333cd4E
-            );
-        operatorFilterRegistry.updateOperators(
-            address(ownedSubscriptionManager),
-            blockedOperatorsList,
-            true
-        );
-        return ownedSubscriptionManager;
-    }
-
     function run() public {
         uint256 chainID = vm.envUint("CHAIN_ID");
         console.log("CHAIN_ID", chainID);
@@ -66,7 +25,6 @@ contract Deploy is Script {
         console2.log("Starting ---");
 
         vm.startBroadcast();
-<<<<<<< HEAD
         address daoFilterMarketAddress = vm.envAddress(
             "SUBSCRIPTION_MARKET_FILTER_ADDRESS"
         );
@@ -75,20 +33,11 @@ contract Deploy is Script {
             "FACTORY_UPGRADE_GATE_OWNER"
         );
 
-=======
-        address subscriptionOwner = vm.envAddress("SUBSCRIPTION_OWNER");
-        console2.log("Setup operators ---");
->>>>>>> 74332c1 (rollback updates to factoryupgradegate)
-        // Add opensea contracts to test
-        OwnedSubscriptionManager ownedSubscriptionManager = setupBlockedOperators(
-                subscriptionOwner
-            );
-
         console2.log("Setup contracts ---");
         DropMetadataRenderer dropMetadata = new DropMetadataRenderer();
         EditionMetadataRenderer editionMetadata = new EditionMetadataRenderer();
         FactoryUpgradeGate factoryUpgradeGate = new FactoryUpgradeGate(
-            subscriptionOwner
+            factoryUpgradeGateOwner
         );
 
         ERC721Drop dropImplementation = new ERC721Drop({

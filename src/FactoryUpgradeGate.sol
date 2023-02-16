a199fe3a800a8d5f01dcacc0915c2ccf2e7f2af2
tbtstl <tysonbattistella@gmail.com>
2023-02-16 10:29:12 -0500

    [feat] implement ownable 2 step in factory upgrade gate

--- src/FactoryUpgradeGate.sol
+++ src/FactoryUpgradeGate.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 import {IFactoryUpgradeGate} from "./interfaces/IFactoryUpgradeGate.sol";
-import "./utils/OwnableSkeleton.sol";
+import "./utils/ownable/Ownable2Step.sol";
 
 /**
 
@@ -19,7 +19,7 @@ import "./utils/OwnableSkeleton.sol";
  */
 
 /// @notice This contract handles gating allowed upgrades for Zora drops contracts
-contract FactoryUpgradeGate is IFactoryUpgradeGate, OwnableSkeleton {
+contract FactoryUpgradeGate is IFactoryUpgradeGate, Ownable2Step {
     /// @notice Private mapping of valid upgrade paths
     mapping(address => mapping(address => bool)) private _validUpgradePaths;
 
@@ -32,39 +32,21 @@ contract FactoryUpgradeGate is IFactoryUpgradeGate, OwnableSkeleton {
     /// @notice Error for when not called from admin
     error Access_OnlyOwner();
 
-    /// @dev Modifier to gate only owner access
-    modifier onlyOwner() {
-        if (msg.sender != owner()) {
-            revert Access_OnlyOwner();
-        }
-
-        _;
-    }
-
     /// @notice Sets the owner and inits the contract
     /// @param _owner owner of the contract
-    constructor(address _owner) {
-        _setOwner(_owner);
-    }
+    constructor(address _owner) Ownable2Step(_owner) {}
 
     /// @notice Ensures the given upgrade path is valid and does not overwrite existing storage slots
     /// @param _newImpl The proposed implementation address
     /// @param _currentImpl The current implementation address
-    function isValidUpgradePath(address _newImpl, address _currentImpl)
-        external
-        view
-        returns (bool)
-    {
+    function isValidUpgradePath(address _newImpl, address _currentImpl) external view returns (bool) {
         return _validUpgradePaths[_newImpl][_currentImpl];
     }
 
     /// @notice Registers a new safe upgrade path for an implementation
     /// @param _newImpl The new implementation
     /// @param _supportedPrevImpls Safe implementations that can upgrade to this new implementation
-    function registerNewUpgradePath(
-        address _newImpl,
-        address[] calldata _supportedPrevImpls
-    ) external onlyOwner {
+    function registerNewUpgradePath(address _newImpl, address[] calldata _supportedPrevImpls) external onlyOwner {
         for (uint256 i = 0; i < _supportedPrevImpls.length; i++) {
             _validUpgradePaths[_newImpl][_supportedPrevImpls[i]] = true;
             emit UpgradePathRegistered(_newImpl, _supportedPrevImpls[i]);
@@ -74,10 +56,7 @@ contract FactoryUpgradeGate is IFactoryUpgradeGate, OwnableSkeleton {
     /// @notice Unregisters an upgrade path, in case of emergency
     /// @param _newImpl the newer implementation
     /// @param _prevImpl the older implementation
-    function unregisterUpgradePath(address _newImpl, address _prevImpl)
-        external
-        onlyOwner
-    {
+    function unregisterUpgradePath(address _newImpl, address _prevImpl) external onlyOwner {
         _validUpgradePaths[_newImpl][_prevImpl] = false;
         emit UpgradePathRemoved(_newImpl, _prevImpl);
     }

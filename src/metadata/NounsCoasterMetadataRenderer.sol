78bd035907cc01db993b8eb19000cd276661b0a4
tbtstl <tysonbattistella@gmail.com>
2023-03-08 19:26:28 -0500

    [attempt] to render

--- src/metadata/NounsCoasterMetadataRenderer.sol
+++ src/metadata/NounsCoasterMetadataRenderer.sol
@@ -20,15 +20,33 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
     /// @notice The metadata renderer settings
     Settings public settings;
 
-    /// @notice The properties chosen from upon generation
+    /// @notice The background properties chosen from upon generation
+    /// [
+    ///   background
+    ///   title
+    ///   corner tag
+    ///   ride
+    /// ]
     Property[] public properties;
+
+    /// @notice the variant dependent properties for nouns
+    /// [
+    ///   0: body variant 1
+    ///   1: body variant 2
+    ///   2: body variant 3
+    ///   3: body variant 4
+    ///   4: accessories variant 1
+    ///   5: accessories variant 2
+    ///   6: accessories variant 3
+    ///   7: accessories variant 4
+    ///   8: head
+    ///   9: expression
+    ///   10: glasses
+    /// ]
+    mapping(uint16 => Property[]) public nounProperties;
 
     /// @notice The IPFS data of all property items
     IPFSGroup[] public ipfsData;
-
-    /// @notice Additional JSON key/value properties for each token.
-    /// @dev While strings are quoted, JSON needs to be escaped.
-    AdditionalTokenProperty[] internal additionalTokenProperties;
 
     ///                                                          ///
     ///                            EVENTS                        ///
@@ -36,9 +54,6 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
 
     /// @notice Emitted when a property is added
     event PropertyAdded(uint256 id, string name);
-
-    /// @notice Additional token properties have been set
-    event AdditionalTokenPropertiesSet(AdditionalTokenProperty[] _additionalJsonProperties);
 
     /// @notice Emitted when the contract image is updated
     event ContractImageUpdated(string prevImage, string newImage);
@@ -87,46 +102,11 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
         settings.token = _token;
     }
 
-    /// @notice The number of items in a property
-    /// @param _propertyId The property id
-    /// @return items array length
-    function itemsCount(uint256 _propertyId) external view returns (uint256) {
-        return properties[_propertyId].items.length;
-    }
-
-    /// @notice The number of items in the IPFS data store
-    /// @return ipfs data array size
-    function ipfsDataCount() external view returns (uint256) {
-        return ipfsData.length;
-    }
-
-    /// @notice Updates the additional token properties associated with the metadata.
-    /// @dev Be careful to not conflict with already used keys such as "name", "description", "properties",
-    function setAdditionalTokenProperties(AdditionalTokenProperty[] memory _additionalTokenProperties) external onlyOwner {
-        delete additionalTokenProperties;
-        for (uint256 i = 0; i < _additionalTokenProperties.length; i++) {
-            additionalTokenProperties.push(_additionalTokenProperties[i]);
-        }
-
-        emit AdditionalTokenPropertiesSet(_additionalTokenProperties);
-    }
-
     /// @notice Adds properties and/or items to be pseudo-randomly chosen from during token minting
     /// @param _names The names of the properties to add
     /// @param _items The items to add to each property
     /// @param _ipfsGroup The IPFS base URI and extension
     function addProperties(string[] calldata _names, ItemParam[] calldata _items, IPFSGroup calldata _ipfsGroup) external onlyOwner {
-        _addProperties(_names, _items, _ipfsGroup);
-    }
-
-    /// @notice Deletes existing properties and/or items to be pseudo-randomly chosen from during token minting, replacing them with provided properties. WARNING: This function can alter or break existing token metadata if the number of properties for this renderer change before/after the upsert. If the properties selected in any tokens do not exist in the new version those token will not render
-    /// @dev We do not require the number of properties for an reset to match the existing property length, to allow multi-stage property additions (for e.g. when there are more properties than can fit in a single transaction)
-    /// @param _names The names of the properties to add
-    /// @param _items The items to add to each property
-    /// @param _ipfsGroup The IPFS base URI and extension
-    function deleteAndRecreateProperties(string[] calldata _names, ItemParam[] calldata _items, IPFSGroup calldata _ipfsGroup) external onlyOwner {
-        delete ipfsData;
-        delete properties;
         _addProperties(_names, _items, _ipfsGroup);
     }
 
@@ -214,30 +194,26 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
     ///                     ATTRIBUTE GENERATION                 ///
     ///                                                          ///
 
-    function _getAttributesForTokenId(uint256 _tokenId) internal view returns (uint16[16] memory) {
-        // Get the number of properties
-        uint256 numProperties = properties.length;
-
+    function _getAttributeIndicesForTokenId(uint256 _tokenId) internal view returns (uint16[16] memory) {
         uint16[16] memory attributes;
-
-        attributes[0] = uint16(numProperties);
 
         uint256 seed = _tokenId;
 
+        // First, add the background properties
+        // Note: Background properties are always the first 5 properties
         unchecked {
-            // For each property:
-            for (uint256 i = 0; i < numProperties; ++i) {
+            // For each bg property:
+            for (uint256 i = 0; i < 5; ++i) {
                 // Get the number of items to choose from
                 uint256 numItems = properties[i].items.length;
 
                 // Use the token's seed to select an item
-                attributes[i + 1] = uint16(seed % numItems);
+                attributes[i] = uint16(seed % numItems);
 
                 // Adjust the seed
                 seed >>= 16;
             }
         }
-
         return attributes;
     }
 
@@ -248,25 +224,19 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
         queryString = string.concat("?contractAddress=", Strings.toHexString(uint256(uint160(address(this))), 20), "&tokenId=", Strings.toString(_tokenId));
 
         // Get the token's generated attributes
-        uint16[16] memory tokenAttributes = _getAttributesForTokenId(_tokenId);
-
-        // Cache the number of properties when the token was minted
-        uint256 numProperties = tokenAttributes[0];
-
-        // Ensure the given token was minted
-        if (numProperties == 0) revert TOKEN_NOT_MINTED(_tokenId);
+        uint16[16] memory tokenAttributes = _getAttributeIndicesForTokenId(_tokenId);
 
         // Get an array to store the token's generated attribtues
-        MetadataBuilder.JSONItem[] memory arrayAttributesItems = new MetadataBuilder.JSONItem[](numProperties);
+        MetadataBuilder.JSONItem[] memory arrayAttributesItems = new MetadataBuilder.JSONItem[](24);
 
         unchecked {
-            // For each of the token's properties:
-            for (uint256 i = 0; i < numProperties; ++i) {
+            // For each of the token's background properties:
+            for (uint256 i = 0; i < 5; ++i) {
                 // Get its name and list of associated items
                 Property memory property = properties[i];
 
                 // Get the randomly generated index of the item to select for this token
-                uint256 attribute = tokenAttributes[i + 1];
+                uint256 attribute = tokenAttributes[i];
 
                 // Get the associated item data
                 Item memory item = property.items[attribute];
@@ -279,6 +249,65 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
                 itemJSON.quote = true;
 
                 queryString = string.concat(queryString, "&images=", _getItemImage(item, property.name));
+            }
+
+            // Next, select the attributes for each noun
+            uint256 seed = _tokenId;
+
+            // For each noun
+            for (uint16 i = 0; i < 4; ++i) {
+                // Cache the properties for this noun.
+                Property[] memory _properties = nounProperties[i];
+                uint16 variant = uint16(seed % 4);
+                seed >>= 16;
+
+                // we know that for each noun, there are 5 total properties that need to be added
+                // properties 1 and 2 are variant dependant, and 3,4,5 are independent
+                uint256 numBodyProperties = _properties[0 + variant].items.length;
+                uint16 bodyIndex = uint16(seed % numBodyProperties);
+
+                // Get the associated itemData
+                Item memory item = _properties[0 + variant].items[bodyIndex];
+
+                // Store the encoded attributes and query string
+                MetadataBuilder.JSONItem memory itemJSON = arrayAttributesItems[4 + (i * 5)];
+                itemJSON.key = _properties[0 + variant].name;
+                itemJSON.value = item.name;
+                itemJSON.quote = true;
+
+                queryString = string.concat(queryString, "&images=", _getItemImage(item, _properties[0 + variant].name));
+
+                // ok, bump the seed and move to property 2
+                seed >>= 16;
+
+                uint256 numAccProperties = _properties[4 + variant].items.length;
+                uint16 accIndex = uint16(seed % numAccProperties);
+
+                item = _properties[4 + variant].items[accIndex];
+
+                // Store the encoded attributes and query string
+                itemJSON = arrayAttributesItems[5 + (i * 5)];
+                itemJSON.key = _properties[4 + variant].name;
+                itemJSON.value = item.name;
+                itemJSON.quote = true;
+
+                queryString = string.concat(queryString, "&images=", _getItemImage(item, _properties[4 + variant].name));
+
+                // Ok, now that the variant dependent items are out of the way, let's loop through the remaining 3 properties
+                for (uint16 j = 0; j < 3; j++) {
+                    seed >>= 16;
+                    uint256 numProperties = _properties[8 + j].items.length;
+                    uint16 index = uint16(seed % numProperties);
+
+                    item = _properties[8 + j].items[index];
+
+                    itemJSON = arrayAttributesItems[5 + (i * 5)];
+                    itemJSON.key = _properties[4 + variant].name;
+                    itemJSON.value = item.name;
+                    itemJSON.quote = true;
+
+                    queryString = string.concat(queryString, "&images=", _getItemImage(item, _properties[8 + j].name));
+                }
             }
 
             resultAttributes = MetadataBuilder.generateJSON(arrayAttributesItems);
@@ -319,17 +348,12 @@ contract NounsCoasterMetadataRenderer is IMetadataRenderer, INounsCoasterMetadat
     function tokenURI(uint256 _tokenId) external view returns (string memory) {
         (string memory _attributes, string memory queryString) = getAttributes(_tokenId);
 
-        MetadataBuilder.JSONItem[] memory items = new MetadataBuilder.JSONItem[](4 + additionalTokenProperties.length);
+        MetadataBuilder.JSONItem[] memory items = new MetadataBuilder.JSONItem[](4);
 
         items[0] = MetadataBuilder.JSONItem({key: MetadataJSONKeys.keyName, value: string.concat(_name(), " #", Strings.toString(_tokenId)), quote: true});
         items[1] = MetadataBuilder.JSONItem({key: MetadataJSONKeys.keyDescription, value: settings.description, quote: true});
         items[2] = MetadataBuilder.JSONItem({key: MetadataJSONKeys.keyImage, value: string.concat(settings.rendererBase, queryString), quote: true});
         items[3] = MetadataBuilder.JSONItem({key: MetadataJSONKeys.keyProperties, value: _attributes, quote: false});
-
-        for (uint256 i = 0; i < additionalTokenProperties.length; i++) {
-            AdditionalTokenProperty memory tokenProperties = additionalTokenProperties[i];
-            items[4 + i] = MetadataBuilder.JSONItem({key: tokenProperties.key, value: tokenProperties.value, quote: tokenProperties.quote});
-        }
 
         return MetadataBuilder.generateEncodedJSON(items);
     }

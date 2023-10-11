790660765835ad5f771ae306ab0cddfaca1fe630
Isabella Smallcombe <isabella@ourzora.com>
2023-10-11 13:10:22 -0400

    feat: add fork tests

--- test/utils/MockContractMetadata.sol
+++ test/utils/MockContractMetadata.sol
@@ -2,25 +2,24 @@
 pragma solidity ^0.8.10;
 
 import {IContractMetadata} from "../../src/interfaces/IContractMetadata.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MockContractMetadata is IContractMetadata {
+contract MockContractMetadata is IContractMetadata, UUPSUpgradeable {
     string public override contractURI;
     string public override contractName;
 
     constructor(string memory _contractURI, string memory _contractName) {
         contractURI = _contractURI;
         contractName = _contractName;
-
     }
 
     function setContractURI(string memory _contractURI) external {
         contractURI = _contractURI;
-
     }
 
     function setContractName(string memory _contractName) external {
         contractName = _contractName;
-
     }
 
+    function _authorizeUpgrade(address _newImplementation) internal override {}
 }
